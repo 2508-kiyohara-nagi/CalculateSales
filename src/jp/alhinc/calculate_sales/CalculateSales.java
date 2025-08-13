cd9a8405aac874ba3f0c95147fdc9cf956ee8467
kiyohara_nagi <kiyohara.nagi@alhinc.jp>
2025-08-13 16:29:04 +0900

    支店定義ファイルのエラー処理２点実装　売上ファイルのエラー処理３点実装

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,7 +34,7 @@ public class CalculateSales {
 	public static void main(String[] args) {
 		// 支店コードと支店名を保持するMap
 		Map<String, String> branchNames = new HashMap<>();
-		
+
 		// 支店コードと売上金額を保持するMap
 		Map<String, Long> branchSales = new HashMap<>();
 
@@ -42,50 +43,76 @@ public class CalculateSales {
 			return;
 		}
 
-		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)		
+		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
 		File[] files = new File(args[0]).listFiles();
 		List<File> rcdFiles =new ArrayList<>();
-		
+
 		//ファイルの数繰り返し処理
 		for (int i = 0;i < files.length; i++) {
-			
+
 			//files[i].getnName();//ファイル名取得　この文字から始まる記号がいる　この文字で終わる　.は正規表現で別の意味
 			if(files[i].getName().matches("^[0-9]{8}\\.rcd$")){
 				rcdFiles.add(files[i]);
+
+			}
+			//売上ファイルのリストのソート
+			Collections.sort(rcdFiles);
+			
+			//売上ファイルが連番になっている確認 繰り返し回数は売上ファイルのリストの数より１つ小さくなるのは次のファイルと比べるため少なくしないと最後の比較で別のエラーがでる。
+			for(int j = 0;j < rcdFiles.size() -1;j++) {
+				//比較する２つのファイルの先頭から数字の８文字を切り出しint型に
+				int former = Integer.parseInt(rcdFiles.get(j).getName().substring(0,8));
+				int latter = Integer.parseInt(rcdFiles.get(j + 1).getName().substring(0,8));
 				
+				//2つのファイルの差が１でない場合エラー処理
+				if((latter - former) != 1) {
+					System.out.println("売上ファイル名が連番になっていません");
+					return;
+				}
 			}
 		}
 		// 売上ファイル読み込み処理
 		for (int i = 0;i< rcdFiles.size(); i++) {
 			BufferedReader br = null;
-	
+
 			try {
 				//rcdFilesにもファイル名あり　デバックで確認
 				File file = new File(args[0], rcdFiles.get(i).getName());
 				FileReader fr = new FileReader(file);
 				br = new BufferedReader(fr);
-				
+
 				//空文字で初期化
 					String line = "";
-					
+
 				//売上ファイル支店コード、売れ上げ金額のリスト
 				List<String> items = new ArrayList<>();
-				
+
 				// 一行ずつ読みこむ
 				while((line = br.readLine()) != null) {
 					items.add(line);
-					
+
 				}
-				
+				//支店コードが定義ファイルにあるか確認
+				if (!branchNames.containsKey(items.get(0))) {
+					System.out.println("<" +  rcdFiles.get(i).getName() + ">の支店コードが不正です");
+					return;
+				}
+
 				long fileSale = Long.parseLong(items.get(1));
-				
-				//読み込んだ売上金額を加算	
+
+				//読み込んだ売上金額を加算
 				Long saleAmount = branchSales.get(items.get(0)) + fileSale;
 				
+				//売上金額１０桁以上の確認
+				if(saleAmount >= 10000000000L) {
+					System.out.println("合計金額が10桁を超えました");
+					return;
+				}
+
 				//mapに加算した値を追加
-				branchSales.put( items.get(0),saleAmount);
-				
-	
+				branchSales.put( items.get(0), saleAmount);
+
+
 			} catch(IOException e) {
 				System.out.println(UNKNOWN_ERROR);
 				return ;
@@ -122,22 +149,35 @@ public class CalculateSales {
 
 		try {
 			File file = new File(path, fileName);
+			 //支店定義ファイルが存在しない場合、コンソールにエラーメッセージを表⽰します。 
+			if(!file.exists()) { 
+				System.out.println("支店定義ファイルが存在しません");
+				return false;
+			   
+			} 
 			//File file = new File("C:\\Users\\trainee1206\\Desktop\\売上集計課題\\branch.lst","branch.lst");
 			FileReader fr = new FileReader(file);
 			br = new BufferedReader(fr);
-			
+
 			// line変数空文字初期化
 			String line = "";
-			
+
 			// 一行ずつ読み込む
 			while((line = br.readLine()) != null) {
 				// ※ここの読み込み処理を変更してください。(処理内容1-2)
 				String[] items = line.split(",");
+				
+				//支店定義ファイルのフォーマット確認
+				if((items.length != 2) || (!items[0].matches("^[0-9]{3}$"))){  
+				    //支店定義ファイルの仕様が満たされていない場合エラメッセージ表示
+				    System.out.println("支店定義ファイルのフォーマットが不正です。");
+				    return false;
+				}
 
 				//Mapに追加する２つの情報をputの引数として指定
 				branchNames.put(items[0], items[1]);
 				branchSales.put(items[0], 0L);
-				
+
 			}
 
 		} catch(IOException e) {
@@ -173,21 +213,21 @@ public class CalculateSales {
 
 		try {
 			File file = new File(path, fileName);
-			
+
 			//ファイルライターの変数fw
 			FileWriter fw = new FileWriter(file);
 
 			//バッファードライターの変数bw
 			bw = new BufferedWriter(fw);
-			
 
 
-			
+
+
 			// 一行ずつ書き込む支店コード、支店名、売上金額
 			for(String key : branchNames.keySet()) {
 				bw.write(key + "," + branchNames.get(key) + "," + branchSales.get(key));
-				bw.newLine(); 
-				
+				bw.newLine();
+
 
 			}
 
@@ -208,6 +248,6 @@ public class CalculateSales {
 			}
 		}
 		return true;
-		}
-
 	}
+
+}
